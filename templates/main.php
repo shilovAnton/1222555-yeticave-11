cea46a23581bee94043bac00e5a109db754d052a
Anton_Shilov <shilov.a.a.1986@gmail.com>
2019-11-03 03:26:10 +0700

    Update main.php

--- templates/main.php
+++ templates/main.php
@@ -6,7 +6,7 @@
             <?php
             foreach($categories as $value): ?>
                 <li class="promo__item promo__item--boards">
-                    <a class="promo__link" href="pages/all-lots.html"><?=$value; ?></a>
+                    <a class="promo__link" href="pages/all-lots.html"><?=htmlspecialchars($value); ?></a>
                 </li> 
             <?php endforeach; ?>
         </ul>
@@ -23,8 +23,8 @@
                         <img src="<?=$val['img']; ?>" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?=$val['category']; ?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$val['name']; ?></a></h3>
+                        <span class="lot__category"><?=htmlspecialchars($val['category']); ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($val['name']); ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount"><?=$val['price']; ?></span>
