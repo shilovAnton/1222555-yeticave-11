69788753ecac3025c9e1c859d9489d35eb52e379
Anton_Shilov <shilov.a.a.1986@gmail.com>
2019-11-03 14:35:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,35 +13,42 @@
             'category' => 'Доски и лыжи',
             'price' => 10999,
             'img' => 'img/lot-1.jpg',
+            'дата истечения' => 2020-02-29
         ],
         [
             'name' => 'DC Ply Mens 2016/2017 Snowboard',
             'category' => 'Доски и лыжи',
             'price' => 159999,
-            'img' => 'img/lot-2.jpg'
-        ], [
+            'img' => 'img/lot-2.jpg',
+            'дата истечения' => 2020-01-01
+        ], 
+        [
             'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
             'category' => 'Крепления',
             'price' => 8000,
-            'img' => 'img/lot-3.jpg'
+            'img' => 'img/lot-3.jpg',
+            'дата истечения' => 2019-12-31
         ],
         [
             'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
             'category' => 'Ботинки',
             'price' => 10999,
-            'img' => 'img/lot-4.jpg'
+            'img' => 'img/lot-4.jpg',
+            'дата истечения' => 2019-12-24
         ],
         [
             'name' => 'Куртка для сноуборда DC Mutiny Charocal',
             'category' => 'Одежда',
             'price' => 7500,
-            'img' => 'img/lot-5.jpg'
+            'img' => 'img/lot-5.jpg',
+            'дата истечения' => 2019-11-29
         ],
         [
             'name' => 'Маска Oakley Canopy',
             'category' => 'Разное',
             'price' => 5400,
-            'img' => 'img/lot-6.jpg'
+            'img' => 'img/lot-6.jpg',
+            'дата истечения' => 2019-11-26
         ]
         ];
     ?>
@@ -52,10 +59,18 @@
     ]);
     $layout_content = include_template('layout.php', ['content' => $page_content,
         'categories' =>  $categories,
-        'title' => 'Главная страница',
+        'title' => 'YetiCave - Главная страница',
         'user_name' => $user_name,
-        'is_auth' => $is_auth
-    ]);
+        'is_auth' => $is_auth,
 
+    ]);
     print($layout_content);
+
+    // Функция подсчета отавшегося времени
+    
+
+
+
+
+
 ?>
