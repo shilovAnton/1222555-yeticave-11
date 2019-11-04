d2d124ba59dbc43ecfc69e12655e0b50914db326
Anton_Shilov <shilov.a.a.1986@gmail.com>
2019-11-04 13:25:08 +0700

    Update main.php

--- templates/main.php
+++ templates/main.php
@@ -17,21 +17,22 @@
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
-            <?php foreach($goods as $key => $val): ?>
+            <?php foreach($goods as $good): ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?=$val['img']; ?>" width="350" height="260" alt="">
+                        <img src="<?=$good['img']; ?>" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?=htmlspecialchars($val['category']); ?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($val['name']); ?></a></h3>
+                        <span class="lot__category"><?=htmlspecialchars($good['category']); ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($good['name']); ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount"></span>
-                                <span class="lot__cost"><? format_as_price_in_rub($val['price']); ?></span>
-                            </div>                            
-                            <div class="<?php if (timer($val['end_date'])[0] > 1): ?>lot__timer timer<?php else :?>timer--finishing<?php endif ?>">                                  
-                                <?= (timer($val['end_date']))[0]?>:<?=(timer($val['end_date']))[1]; ?>
+                                <span class="lot__cost"><? format_as_price_in_rub($good['price']); ?></span>
+                            </div>    
+                            <?php $timer = timer($good['end_date']); ?>                      
+                            <div class="lot__timer timer <?php if ($timer[0] < 1): ?> timer--finishing<?php endif;?>">
+                                <?= $timer[0]?>:<?=$timer[1]; ?>
                             </div>                               
                         </div>
                     </div>
