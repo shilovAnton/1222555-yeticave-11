e307693d650352927073565e5129339d10b147d2
Anton_Shilov <shilov.a.a.1986@gmail.com>
2019-11-06 23:26:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,73 +1,75 @@
 <?php
-    require_once('helpers.php');
+require_once('helpers.php');
 
-    date_default_timezone_set('Asia/Novosibirsk');
+date_default_timezone_set('Asia/Novosibirsk');
 
-    $user_name = 'Антон'; // укажите здесь ваше имя
-    
-    $is_auth = rand(0, 1);
+$user_name = 'Антон'; // укажите здесь ваше имя
 
-    $categories = ["Доски и лыжи","Крепления","Ботинки","Одежда","Инструменты","Разное"];//Одномерный массив 
-    
-    $goods = [   //Двумерный массив
-        [
-            'name' => '2014 Rossignol District Snowboard',
-            'category' => 'Доски и лыжи',
-            'price' => 10999,
-            'img' => 'img/lot-1.jpg',
-            'end_date' => '2019-11-05'
-        ],
-        [
-            'name' => 'DC Ply Mens 2016/2017 Snowboard',
-            'category' => 'Доски и лыжи',
-            'price' => 159999,
-            'img' => 'img/lot-2.jpg',
-            'end_date' => '2019-11-06'
-        ], 
-        [
-            'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-            'category' => 'Крепления',
-            'price' => 8000,
-            'img' => 'img/lot-3.jpg',
-            'end_date' => '2019-12-31'
-        ],
-        [
-            'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
-            'category' => 'Ботинки',
-            'price' => 10999,
-            'img' => 'img/lot-4.jpg',
-            'end_date' => '2019-12-24'
-        ],
-        [
-            'name' => 'Куртка для сноуборда DC Mutiny Charocal',
-            'category' => 'Одежда',
-            'price' => 7500,
-            'img' => 'img/lot-5.jpg',
-            'end_date' => '2019-11-29'
-        ],
-        [
-            'name' => 'Маска Oakley Canopy',
-            'category' => 'Разное',
-            'price' => 5400,
-            'img' => 'img/lot-6.jpg',
-            'end_date' => '2019-11-26'
-        ]
-        ];
-    ?>
+$is_auth = rand(0, 1);
+
+$categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];//Одномерный массив
+
+$goods = [   //Двумерный массив
+    [
+        'name' => '2014 Rossignol District Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => 10999,
+        'img' => 'img/lot-1.jpg',
+        'end_date' => '2019-11-05'
+    ],
+    [
+        'name' => 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => 159999,
+        'img' => 'img/lot-2.jpg',
+        'end_date' => '2019-11-06'
+    ],
+    [
+        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'price' => 8000,
+        'img' => 'img/lot-3.jpg',
+        'end_date' => '2019-12-31'
+    ],
+    [
+        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'price' => 10999,
+        'img' => 'img/lot-4.jpg',
+        'end_date' => '2019-12-24'
+    ],
+    [
+        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'category' => 'Одежда',
+        'price' => 7500,
+        'img' => 'img/lot-5.jpg',
+        'end_date' => '2019-11-29'
+    ],
+    [
+        'name' => 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'price' => 5400,
+        'img' => 'img/lot-6.jpg',
+        'end_date' => '2019-11-26'
+    ]
+];
+?>
 
 <?php // Подключение шаблонов
-    $page_content = include_template('main.php', ['goods' => $goods,
-         'categories' =>  $categories,
-         
-        // 'dt' => $time_count
-    ]);
-    $layout_content = include_template('layout.php', ['content' => $page_content,
-        'categories' =>  $categories,
-        'title' => 'YetiCave - Главная страница',
-        'user_name' => $user_name,
-        'is_auth' => $is_auth,//Рандомная функция
-         
-    ]);
-    print($layout_content);
+$page_content = include_template('main.php', [
+    'goods' => $goods,
+    'categories' => $categories,
+
+    // 'dt' => $time_count
+]);
+$layout_content = include_template('layout.php', [
+    'content' => $page_content,
+    'categories' => $categories,
+    'title' => 'YetiCave - Главная страница',
+    'user_name' => $user_name,
+    'is_auth' => $is_auth,//Рандомная функция
+
+]);
+print($layout_content);
 
 ?>
