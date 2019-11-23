36b5d90e3e675bc3999e82e7afa2f186ad856cc2
Anton_Shilov <shilov.a.a.1986@gmail.com>
2019-11-24 01:10:37 +0700

    Create categories.php
    Работаем со строкой запроса

---
+++ categories.php
@@ -0,0 +1,10 @@
+<?php
+require_once('mysqli_connect.php');
+
+$query_categories = "SELECT * FROM categories";
+$result_categories = mysqli_query($mysqli_connect, $query_categories);  // функция для выполнения любых SQL запросов
+if ($result_categories) {
+    $categories = mysqli_fetch_all($result_categories, MYSQLI_ASSOC);  // функция для создания масива
+} else {
+    show_error($mysqli_connect);
+}
