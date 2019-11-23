333d8a90852f32a4d9a1305e55a6d24885665989
Anton_Shilov <shilov.a.a.1986@gmail.com>
2019-11-24 01:11:39 +0700

    Create mysqli_connect.php

---
+++ mysqli_connect.php
@@ -0,0 +1,21 @@
+<?php
+// Подключение БД
+$mysqli_connect = mysqli_connect("localhost", "root", "", "yeticave");//Подключение БД, ресурс соединения
+if ($mysqli_connect == false) {
+    print("Ошибка подключения: " . mysqli_connect_error());
+    die;
+}
+
+mysqli_set_charset($mysqli_connect, "utf8");  // Установить кодировку
+
+/**
+ * Выводит ошибку если соединение с БД не установлено.
+ * @param $mysqli_connect
+ *
+ */
+function show_error($mysqli_connect)
+{
+    $error = mysqli_error($mysqli_connect);
+    print("Ошибка MySQL: " . $error);
+    die;
+}
