346db7350496be1509b53d1a1e7f9a829c3bda6c
Anton_Shilov <shilov.a.a.1986@gmail.com>
2019-12-19 18:01:48 +0700

    Update sign-up.php

--- templates/sign-up.php
+++ templates/sign-up.php
@@ -1,30 +1,31 @@
-<pre><?php var_dump($errors_sign_ap); ?></pre>
-<form class="form container<?php if (count($errors_sign_ap) > 0): ?> form--invalid<?php endif; ?>"
+<pre><?php var_dump($errors_sign_up); ?></pre>
+<pre><?php var_dump($_POST); ?></pre>
+<form class="form container<?php if (count($errors_sign_up) > 0): ?> form--invalid<?php endif; ?>"
       action="sign-up.php" method="post" autocomplete="off">
     <h2>Регистрация нового аккаунта</h2>
-    <div class="form__item<?php if (isset($errors_sign_ap['email'])): ?> form__item--invalid<?php endif; ?>">
+    <div class="form__item<?php if (isset($errors_sign_up['email'])): ?> form__item--invalid<?php endif; ?>">
         <label for="email">E-mail <sup>*</sup></label>
         <input id="email" type="text" name="email" placeholder="Введите e-mail" value="<?=getPostVal('email'); ?>">
-        <span class="form__error"><?= $errors_sign_ap['email']; ?></span>
+        <span class="form__error"><?= $errors_sign_up['email']; ?></span>
     </div>
-    <div class="form__item<?php if (isset($errors_sign_ap['password'])): ?> form__item--invalid<?php endif; ?>">
+    <div class="form__item<?php if (isset($errors_sign_up['password'])): ?> form__item--invalid<?php endif; ?>">
         <label for="password">Пароль <sup>*</sup></label>
         <input id="password" type="password" name="password" placeholder="Введите пароль" value="<?=getPostVal('password'); ?>">
-        <span class="form__error"><?= $errors_sign_ap['password']; ?></span>
+        <span class="form__error"><?= $errors_sign_up['password']; ?></span>
     </div>
-    <div class="form__item<?php if (isset($errors_sign_ap['name'])): ?> form__item--invalid<?php endif; ?>">
+    <div class="form__item<?php if (isset($errors_sign_up['name'])): ?> form__item--invalid<?php endif; ?>">
         <label for="name">Имя <sup>*</sup></label>
         <input id="name" type="text" name="name" placeholder="Введите имя" value="<?=getPostVal('name'); ?>">
-        <span class="form__error"><?= $errors_sign_ap['name']; ?></span>
+        <span class="form__error"><?= $errors_sign_up['name']; ?></span>
     </div>
-    <div class="form__item<?php if (isset($errors_sign_ap['message'])): ?> form__item--invalid<?php endif; ?>">
+    <div class="form__item<?php if (isset($errors_sign_up['message'])): ?> form__item--invalid<?php endif; ?>">
         <label for="message">Контактные данные <sup>*</sup></label>
         <textarea id="message" name="message" placeholder="Напишите как с вами связаться"><?=getPostVal('message'); ?></textarea>
-        <span class="form__error"><?= $errors_sign_ap['message']; ?></span>
+        <span class="form__error"><?= $errors_sign_up['message']; ?></span>
     </div>
-    <?php if (count($errors_sign_ap) > 0): ?>
+    <?php if (count($errors_sign_up) > 0): ?>
         <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
     <?php endif; ?>
     <button type="submit" class="button">Зарегистрироваться</button>
-    <a class="text-link" href="#">Уже есть аккаунт</a>
+    <a class="text-link" href="login.php">Уже есть аккаунт</a>
 </form>
