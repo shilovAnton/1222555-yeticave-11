78675bf9fa1f919b0b4909dbeb17e68bd3bd44f8
Anton_Shilov <shilov.a.a.1986@gmail.com>
2019-12-21 02:12:01 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,5 @@
 <?php
-require_once('helpers.php'); //Подключаем файлы
-require_once('mysqli_connect.php');
-require_once('categories.php');
-require_once('user.php');
-date_default_timezone_set('Asia/Novosibirsk');
+require_once('attach_file.php');
 
 $errors_login = [];
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
