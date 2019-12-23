76ad2030e5ee2e945fa7bd61f1943048864a784c
Anton_Shilov <shilov.a.a.1986@gmail.com>
2019-12-23 22:57:28 +0700

    Update search.php

--- templates/search.php
+++ templates/search.php
@@ -1,28 +1,31 @@
-
 <div class="container">
     <section class="lots">
-        <h2>Результаты поиска по запросу «<span><?= htmlspecialchars($_GET['search']); ?></span>»</h2>
+        <?php if(!empty($lots)): ?>
+        <h2>Результаты поиска по запросу «<span><?= htmlspecialchars($search_valid); ?></span>»</h2>
+        <?php else: ?>
+        <h2>Ничего не найдено по вашему запросу</h2>
+        <?php endif; ?>
         <ul class="lots__list">
-            <?php foreach ($lots as $lot => $key): ?>
+            <?php foreach ($lots as $lot): ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?= $lots[$lot]['img']; ?>" width="350" height="260" alt="">
+                        <img src="<?= $lot['img']; ?>" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?= htmlspecialchars($lots[$lot]['category_name']); ?></span>
+                        <span class="lot__category"><?= htmlspecialchars($lot['category_name']); ?></span>
                         <h3 class="lot__title"><a class="text-link"
-                                                  href="lot.php?id=<?= $lots[$lot]['id']; ?>"><?= htmlspecialchars($lots[$lot]['lot_name']); ?></a></h3>
+                                                  href="lot.php?id=<?= $lot['id']; ?>"><?= htmlspecialchars($lot['lot_name']); ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
-                                <span class="lot__amount">праптмтмтм</span>
-                                <span class="lot__cost"><?php if(!empty($lots[$lot]['current_price'])): ?>
-                                        <?= format_as_price_in_rub($lots[$lot]['current_price']); ?>
+                                <span class="lot__amount">Цена</span>
+                                <span class="lot__cost"><?php if(!empty($lot['current_price'])): ?>
+                                        <?= format_as_price_in_rub($lot['current_price']); ?>
                                     <?php else: ?>
-                                        <?= format_as_price_in_rub($lots[$lot]['initial_price']); ?>
+                                        <?= format_as_price_in_rub($lot['initial_price']); ?>
                                     <?php endif; ?>
                                 </span>
                             </div>
-                            <?php $timer = timer($lots[$lot]['dt_end']); ?>
+                            <?php $timer = timer($lot['dt_end']); ?>
                             <div class="lot__timer timer <?php if ($timer[0] < 1): ?>timer--finishing<?php endif; ?>">
                                 <?= $timer[0] ?> : <?= $timer[1]; ?>
                             </div>
@@ -33,11 +36,17 @@
         </ul>
     </section>
     <ul class="pagination-list">
-        <li class="pagination-item pagination-item-prev"><a>Назад</a></li>
-        <li class="pagination-item pagination-item-active"><a>1</a></li>
-        <li class="pagination-item"><a href="#">2</a></li>
-        <li class="pagination-item"><a href="#">3</a></li>
-        <li class="pagination-item"><a href="#">4</a></li>
-        <li class="pagination-item pagination-item-next"><a href="#">Вперед</a></li>
+        <?php if ($cur_page != 1): ?>
+            <li class="pagination-item pagination-item-prev"><a
+                    href="/search.php?page=<?= $cur_page - 1; ?>&search=<?= $search_valid; ?>">Назад</a></li>
+        <?php endif; ?>
+        <?php foreach ($pages as $page): ?>
+            <li class="pagination-item <?php if ($cur_page == $page): ?>pagination-item-active<?php endif; ?>"><a
+                    href="/search.php?page=<?= $page; ?>&search=<?= $search_valid; ?>"><?= $page; ?></a></li>
+        <?php endforeach; ?>
+        <?php if ($cur_page != count($pages)): ?>
+            <li class="pagination-item pagination-item-next"><a
+                    href="/search.php?page=<?= $cur_page + 1; ?>&search=<?= $search_valid; ?>">Вперед</a></li>
+        <?php endif; ?>
     </ul>
 </div>
