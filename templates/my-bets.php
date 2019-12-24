a69d73986c0a1180439c8b431a93bda1c82bdac5
Anton_Shilov <shilov.a.a.1986@gmail.com>
2019-12-25 03:27:33 +0700

    Update my-bets.php

--- templates/my-bets.php
+++ templates/my-bets.php
@@ -1,23 +1,47 @@
-<pre><?= var_dump($my_bids); ?></pre>
 <section class="rates container">
     <h2>Мои ставки</h2>
     <table class="rates__list">
         <?php foreach ($my_bids as $bid): ?>
-            <tr class="rates__item">
+            <?php $timer = timer($bid['dt_end']);
+            $is_winner = (int)$bid['user_id_winner'] === $_SESSION['user']['id'];
+            $is_end = ($timer[0] < 1) && ($timer[1] < 1);
+
+            $is_finishing = ($timer[0] < 1) && ($timer[1] > 0);
+
+            $timer_class = 'timer';
+            $timer_text = $timer[0] . ':' . $timer[1];
+
+            $rates_item_class = 'rates__item';
+
+            if ($is_winner) {
+                $rates_item_class .= ' rates__item--win';
+                $timer_class .= ' timer--win';
+                $timer_text = 'Ставка выиграла';
+            } elseif ($is_finishing) {
+                $timer_class .= ' timer--finishing';
+            } elseif ($is_end) {
+                $rates_item_class .= ' rates__item--end';
+                $timer_class .= ' timer--end';
+                $timer_text = 'Торги окончены';
+            }
+            ?>
+            <tr class="<?= $rates_item_class; ?>">
                 <td class="rates__info">
                     <div class="rates__img">
                         <img src="<?= $bid['img']; ?>" width="54" height="40" alt="<?= $bid['lot_name']; ?>">
                     </div>
-                    <h3 class="rates__title"><a href="lot.php?id=<?= $bid['id']; ?>"><?= $bid['lot_name']; ?></a></h3>
+                    <h3 class="rates__title"><a
+                            href="<?php if (!$is_end): ?>lot.php?id=<?= $bid['id']; ?><?php endif; ?>"><?= $bid['lot_name']; ?></a>
+                    </h3>
                 </td>
                 <td class="rates__category">
                     <?= $bid['category_name']; ?>
                 </td>
 
                 <td class="rates__timer">
-                    <?php $timer = timer($bid['dt_end']); ?>
-                    <div class="timer<?php if ($timer[0] < 1): ?> timer--finishing<?php endif; ?>">
-                        <?= $timer[0] ?> : <?= $timer[1]; ?>
+                    <div
+                        class="<?= $timer_class; ?>">
+                        <?= $timer_text; ?>
                     </div>
                 </td>
 
